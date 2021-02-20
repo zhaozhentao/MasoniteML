0c2f649119ec0560f6808d8375c5b958ca9fd136
zhaozhentao <qq344696734@gmail.com>
2021-02-20 20:15:51 +0800

    加载tensorflow model

--- app/providers/DLModelProvider.py
+++ app/providers/DLModelProvider.py
@@ -1,5 +1,6 @@
 """A DLProvider Service Provider."""
 
+import tensorflow as tf
 from masonite.provider import ServiceProvider
 
 
@@ -7,15 +8,17 @@ class DLModelProvider(ServiceProvider):
     """Provides Services To The Service Container."""
 
     wsgi = True
-    __counter = 0
+    __detect_model = None
+    __recognition_model = None
 
     def register(self):
         self.app.bind('ai', self)
+        __detect_model = tf.keras.models.load_model('zc.h5')
+        __recognition_model = tf.keras.models.load_model('plate.h5')
 
     def boot(self):
         """Boots services required by the container."""
         pass
 
-    def count(self):
-        self.__counter += 1
-        return self.__counter
+    def model(self):
+        print('model')
